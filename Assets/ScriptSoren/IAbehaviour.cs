d08c8f804a3598e335862f23cd989eecb57e084d
TerrierBrakk <felipeortega94@gmail.com>
2021-04-15 17:31:40 -0500

    IaTest

---
+++ Assets/ScriptSoren/IAbehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IAbehaviour : MonoBehaviour
+{
+
+    public Transform playerLoc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(playerLoc);
+
+    }
+
+    // Update is called once per frame
+    void Update()   
+    {
+        
+    }
+}
