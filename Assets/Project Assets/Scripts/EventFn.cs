f793a462ec5a935a4f3b05beb56da95407385902
darketomaly <darketomaly@gmail.com>
2021-05-03 16:52:53 -0500

    redefined title screen concept
    navigation due

---
+++ Assets/Project Assets/Scripts/EventFn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventFn : MonoBehaviour {
+
+    public static EventFn instance;
+    bool move = false;
+
+    void Awake() {
+
+        instance = this;
+    }
+
+    public static void LoadFunctions(string _function, string[] _data) {
+
+        switch (_function) {
+
+            case "Sumar": {
+
+                    Debug.Log($"Sumar :: {int.Parse(_data[0]) + int.Parse(_data[1])}");
+                }
+                break;
+
+            case "Click": {
+
+                    Debug.Log("Click");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
