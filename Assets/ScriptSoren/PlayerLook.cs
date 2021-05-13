5673bd6f7ecb6a05e197c5f22a21e5902d367b00
TerrierBrakk <felipeortega94@gmail.com>
2021-05-13 07:25:02 -0500

    IA Behaviour
    Se agregó dos scripts para la ia, Desaparece si no lo ves dentro de un angulo de 60 grados a una distancia de 15 metros.

---
+++ Assets/ScriptSoren/PlayerLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Transform slender;
+
+    public bool isLooking;
+    public static float timeNotLooking;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(slender.position, transform.position);
+        float angle = Vector3.Angle(transform.forward, slender.position - transform.position);
+
+        if (angle < 60 && distance < 15)
+        {
+            timeNotLooking = 0;
+        }
+        else
+        {
+            isLooking = false;
+            timeNotLooking += Time.deltaTime;
+        }
+
+    }
+
+}
